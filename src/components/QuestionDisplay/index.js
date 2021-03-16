3b08eaeeee51423b74627d9d849185dc957393cb
Sergey Bolotnikov <sbolotnikov@gmail.com>
2021-03-16 08:42:49 -0400

    fix display menu and test view

--- src/components/QuestionDisplay/index.js
+++ src/components/QuestionDisplay/index.js
@@ -5,7 +5,7 @@ import "./style.css";
 const QuestionDisplay = props => {
 
     return (
-            <div style={{height:'100%',width:'100%',backgroundImage: `url(${props.background})`, backgroundRepeat: "no-repeat", backgroundSize: 'cover'}}>
+            <div style={{height:'100vh',width:'100%',backgroundImage: `url(${props.background})`, backgroundRepeat: "no-repeat", backgroundSize: 'cover'}}>
                 {props.info.layout === 'box' ? <LayoutBox type={props.info.correct} vis={props.vis} question={props.question} answers={props.answers} checkedMarks={props.checkedMarks} onChange={(ch) => { props.onChange(ch) }} />
                     : <div style={{ display: 'none' }}></div>}
                 {props.info.layout === 'simple' ? <LayoutSimple info={props.info} vis={props.vis} question={props.question} answers={props.answers} checkedMarks={props.checkedMarks} onChange={(ch) => { props.onChange(ch) }} />
