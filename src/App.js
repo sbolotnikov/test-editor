c5e391f1412e5c396749ead8bbc802d133712ee0
Sergey Bolotnikov <sbolotnikov@gmail.com>
2021-03-16 05:41:39 -0400

    trying HashRoute

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { HashRouter as Router, Switch, Route } from "react-router-dom"
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Components
