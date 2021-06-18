afa5d06a858d96d43ffc176fce8290dd49bf0f46
Sergey Bolotnikov <sbolotnikov@gmail.com>
2021-06-18 17:21:56 -0400

    fix image close sign

--- src/components/ZoomImage.js
+++ src/components/ZoomImage.js
@@ -2,12 +2,13 @@ import React from "react"
 
 export default function ZoomImage(props) {
 // for simple layout it allows to zoom in to picture in the question displayed
+
   return (
 
-    <div style={{ width: '100vw', height: '100vh', position: 'absolute', top: window.pageYOffset, left: 0, display: 'flex', justifyContent: 'center', alignItems: 'center', zIndex: '1900', backgroundColor: 'rgba(105,105,105, .7)' }} >
-      <div style={{ margin: 'auto', maxWidth: '600px', border: '1px solid gray', borderRadius: '.25rem', padding: '2%', display:'flex' }}>    
+    <div style={{ width: '100vw', height: '100vh', position: 'absolute', top: window.pageYOffset, left: 0, display: 'flex', justifyContent: 'center', alignItems: 'center', zIndex: '2000', backgroundColor: 'rgba(105,105,105, .7)' }} >
+      <div style={{ margin: 'auto', maxWidth: '600px', border: '1px solid gray', borderRadius: '.25rem', padding: '2%'}}>    
           <img src={props.img} style={{ width: '100%', visibility: 'visible' }} alt='zooming in' />
-          <div className="closeTag" style={{ position: 'relative', top: '2%', right: '4%', visibility: 'visible' }} onClick={(e) =>{ props.closeModal(true)}}><img src={process.env.PUBLIC_URL + "/icons/close.svg"} alt="close" style={{ width: 'max(1.2vw,12px)', height: 'max(1.2vw,12px)' }} /></div>
+          <div className="closeTag"  onClick={(e) =>{ props.closeModal(true)}}><img src={process.env.PUBLIC_URL + "/icons/close.svg"} alt="close" style={{ float:'right', width: 'max(1.2vw,12px)', height: 'max(1.2vw,12px)',color:"red" }} /></div>
       </div>
 
     </div>
